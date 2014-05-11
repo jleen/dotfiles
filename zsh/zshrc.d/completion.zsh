5dd23be48f8e54c0db65b6f114086eb3ae19f34f
John Leen <jleen@saturnvalley.org>
2014-05-11 09:38:24 -0700

    Menu completion

--- zsh/zshrc.d/completion.zsh
+++ zsh/zshrc.d/completion.zsh
@@ -1,3 +1,4 @@
 # Case-insensitive, partial-word, and substring completion, cribbed from
 # oh-my-zsh.
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+zstyle ':completion:*' menu select
