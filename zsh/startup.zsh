d165187d79b762f5a2d520d5af3c7668cdde8051
John Leen <jleen@saturnvalley.org>
2015-11-27 13:31:37 -0800

    A place for local config

---
+++ zsh/startup.zsh
@@ -0,0 +1,19 @@
+# zshenv sets up environment variables that we want even if this is a
+# noninteractive shell, e.g. variables that we'd like to have available in
+# processes launched by wrapper scripts.
+
+# This is for zsh but it's useful to have it bash-compatible (so it can be
+# sourced from .xsession, for example).
+if [[ -n $BASH_VERSION ]]; then
+  export SVCONFIGDIR=`dirname $(dirname ${BASH_ARGV[0]})`
+else
+   export SVCONFIGDIR="${0:h:h}"
+fi
+
+# For root shells, don't do anything fancy.
+if [[ $UID != 0  && -z $SUDO_USER ]]; then
+  source $SVCONFIGDIR/zsh/zshenv
+  [[ -f $SVCONFIGDIR/local/zshenv ]] && source $SVCONFIGDIR/local/zshenv
+  [[ -o i ]] && source $SVCONFIGDIR/zsh/zshrc
+  [[ -f $SVCONFIGDIR/local/zshrc ]] && source $SVCONFIGDIR/local/zshrc
+fi
