b82a63fc38e0589dc44dd4a6ee6d85574c0bddaa
John Leen <jleen@saturnvalley.org>
2020-05-14 17:41:34 -0400

    Windows deserves a pretty prompt too

---
+++ cmder/config/prompt.lua
@@ -0,0 +1,73 @@
+-- from cmder\vendor\clink.lua
+-- adapted from from clink-completions' git.lua
+local function get_git_dir(path)
+
+    -- return parent path for specified entry (either file or directory)
+    local function pathname(path)
+        local prefix = ""
+        local i = path:find("[\\/:][^\\/:]*$")
+        if i then
+            prefix = path:sub(1, i-1)
+        end
+
+        return prefix
+    end
+
+    -- Checks if provided directory contains git directory
+    local function has_git_dir(dir)
+        return clink.is_dir(dir..'/.git') and dir..'/.git'
+    end
+
+    local function has_git_file(dir)
+        local gitfile = io.open(dir..'/.git')
+        if not gitfile then return false end
+
+        local git_dir = gitfile:read():match('gitdir: (.*)')
+        gitfile:close()
+
+        return git_dir and dir..'/'..git_dir
+    end
+
+    -- Set default path to current directory
+    if not path or path == '.' then path = clink.get_cwd() end
+
+    -- Calculate parent path now otherwise we won't be
+    -- able to do that inside of logical operator
+    local parent_path = pathname(path)
+
+    return has_git_dir(path)
+        or has_git_file(path)
+        -- Otherwise go up one level and make a recursive call
+        or (parent_path ~= path and get_git_dir(parent_path) or nil)
+end
+
+-- from cmder\vendor\clink.lua
+local function get_git_branch(git_dir)
+    git_dir = git_dir or get_git_dir()
+
+    -- If git directory not found then we're probably outside of repo
+    -- or something went wrong. The same is when head_file is nil
+    local head_file = git_dir and io.open(git_dir..'/HEAD')
+    if not head_file then return end
+
+    local HEAD = head_file:read()
+    head_file:close()
+
+    -- if HEAD matches branch expression, then we're on named branch
+    -- otherwise it is a detached commit
+    local branch_name = HEAD:match('ref: refs/heads/(.+)')
+
+    return branch_name or 'HEAD detached at '..HEAD:sub(1, 7)
+end
+
+function sv_prompt_filter()
+    local git_prompt = ""
+    local git_branch = get_git_branch()
+    if git_branch then
+        git_prompt = ":" .. git_branch
+    end
+    clink.prompt.value = "\x1b[42m" .. clink.get_cwd() .. "\x1b[0m" .. git_prompt .. "\x1b[32m\x1b[0m"
+end
+
+clink.prompt.filters = {}
+clink.prompt.register_filter(sv_prompt_filter, 100)
