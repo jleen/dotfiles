74d715eb9a8413c1a34f7baf758ae3bd6b8a2cf5
John Leen <jleen@salesforce.com>
2020-09-28 17:27:09 -0400

    Detangle redundant formatting

--- zsh/zshrc.d/prompt.zsh
+++ zsh/zshrc.d/prompt.zsh
@@ -51,7 +51,7 @@ else
     local prefix="%{$tput_setaf_0$tput_bold%}$SHELLPREFIX%{$tput_sgr0%}"
   fi
   local cruft="%{${screen_clear}${xterm}${shellprefix}%}"
-  local color_on=${SV_SIGIL_COLOR:-%{$tput_setaf$tput_bold%}}
+  local color_on="%{$tput_setaf$tput_bold%}"
   local color_off="%{$tput_sgr0%}"
   setopt prompt_subst
   PROMPT="${cruft}${prefix}${color_on}${short_host}${screen}${color_off} $pre_pwd\${SV_SPECIAL_PROMPT}\${SV_PWD_PROMPT:-%~}$post_pwd$caret "
