51d9bb5a06c45ec743951c6d483cf14457e9bc6a
John Leen <jleen@saturnvalley.org>
2021-04-12 20:40:49 -0400

    Fix Powerline when no dir aliases

--- zsh/zshrc.d/powerline.zsh
+++ zsh/zshrc.d/powerline.zsh
@@ -6,7 +6,8 @@
 if [[ -n "$SV_POWERLINE_GO_BIN" ]]; then
 
     function powerline_precmd() {
-        eval "$($SV_POWERLINE_GO_BIN -condensed -error $? -static-prompt-indicator -modules venv,host,ssh,cwd,perms,jobs,exit,root -modules-right git,hg,svn -shell zsh -eval -theme ${SV_POWERLINE_GO_THEME:-default} -path-aliases $SV_POWERLINE_EXTRA_ARGS)"
+        # TODO: Avoid the need for a nonsense qqq string.
+        eval "$($SV_POWERLINE_GO_BIN -condensed -error $? -static-prompt-indicator -modules venv,host,ssh,cwd,perms,jobs,exit,root -modules-right git,hg,svn -shell zsh -eval -theme ${SV_POWERLINE_GO_THEME:-default} -path-aliases ${SV_POWERLINE_EXTRA_ARGS:-qqqqqqqqq=q})"
         if [[ -n "$SV_PROMPT_SIGIL_POWERLINE" ]]; then
             PS1=`echo $PS1 | sed -e s/%m/$SV_PROMPT_SIGIL_POWERLINE/`
         elif [[ -n "$SV_PROMPT_SIGIL" ]]; then
