5b84e5ec95d0d32d81c45227e891eca8a764ce49
John Leen <jleen@saturnvalley.org>
2022-01-04 20:51:35 -0500

    Configure exa location

--- zsh/zshrc.d/exa.zsh
+++ zsh/zshrc.d/exa.zsh
@@ -1,12 +1,14 @@
-if [ -x ~/.cargo/bin/exa ]; then
+[[ -z $SV_EXA_BIN ]] && [[ -x ~/.cargo/bin/exa ]] && SV_EXA_BIN=~/.cargo/bin/exa
+
+if [ -x $SV_EXA_BIN ]; then
     unalias ls
     function ls () {
         case $1 in
-            -l)  shift ; ~/.cargo/bin/exa -l  $* ;;
-            -a)  shift ; ~/.cargo/bin/exa -a  $* ;;
-            -la) shift ; ~/.cargo/bin/exa -la $* ;;
+            -l)  shift ; $SV_EXA_BIN -l  $* ;;
+            -a)  shift ; $SV_EXA_BIN -a  $* ;;
+            -la) shift ; $SV_EXA_BIN -la $* ;;
             -*)  /bin/ls                      $* ;;
-            *)           ~/.cargo/bin/exa     $* ;;
+            *)           $SV_EXA_BIN     $* ;;
         esac
     }
 fi
