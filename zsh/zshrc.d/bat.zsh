80b4b9f389f616eab3d55467c3cb6413c0da71ca
John Leen <jleen@saturnvalley.org>
2022-06-06 09:34:19 -0400

    Quick, to the bat cache!

--- zsh/zshrc.d/bat.zsh
+++ zsh/zshrc.d/bat.zsh
@@ -1,3 +1,4 @@
 [[ -z $SV_BAT_BIN ]] && [[ -x ~/.cargo/bin/bat ]] && SV_BAT_BIN=~/.cargo/bin/bat
 
 [[ -x $SV_BAT_BIN ]] && alias cat=$SV_BAT_BIN
+alias batcache="bat cache --build --source $SVCONFIGDIR/bat"
