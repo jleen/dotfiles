4133e0d1afbb7c4e3648b7bd0bd491473f3d4fef
John <jleen@salesforce.com>
2022-08-18 17:18:54 -0400

    Look for exa in default locations

--- zsh/zshrc.d/exa.zsh
+++ zsh/zshrc.d/exa.zsh
@@ -1,14 +1,15 @@
-[[ -z $SV_EXA_BIN ]] && [[ -x ~/.cargo/bin/exa ]] && SV_EXA_BIN=~/.cargo/bin/exa
-
-if [ -x $SV_EXA_BIN ]; then
-    unalias ls
-    function ls () {
-        case $1 in
-            -l)  shift ; $SV_EXA_BIN -l  $* ;;
-            -a)  shift ; $SV_EXA_BIN -a  $* ;;
-            -la) shift ; $SV_EXA_BIN -la $* ;;
-            -*)  /bin/ls                      $* ;;
-            *)           $SV_EXA_BIN     $* ;;
-        esac
-    }
-fi
+for exa_bin in "$SV_EXA_BIN" ./cargo/bin/exa /usr/local/bin/exa; do
+    if [[ -x $exa_bin ]]; then
+        unalias ls
+        function ls () {
+            case $1 in
+                -l)  shift ; $exa_bin -l  $* ;;
+                -a)  shift ; $exa_bin -a  $* ;;
+                -la) shift ; $exa_bin -la $* ;;
+                -*)  /bin/ls                      $* ;;
+                *)           $exa_bin     $* ;;
+            esac
+        }
+        break
+    fi
+done
