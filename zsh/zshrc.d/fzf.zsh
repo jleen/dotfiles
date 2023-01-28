7a8d465ece46ebd0b84958a93378c6b1567c98e9
John Leen <jleen@saturnvalley.org>
2023-01-27 20:36:36 -0500

    Integrated fzf integration

--- zsh/zshrc.d/fzf.zsh
+++ zsh/zshrc.d/fzf.zsh
@@ -1,8 +1,17 @@
+# Maybe the whole package is installed.
 if [[ -z "$SV_FZF_DIR" && -d /usr/share/doc/fzf/examples ]]; then
     SV_FZF_DIR=/usr/share/doc/fzf/examples
 fi
 
+# If we found the package, use its shell integration.
 if [[ -n "$SV_FZF_DIR" ]]; then
     source $SV_FZF_DIR/key-bindings.zsh
     source $SV_FZF_DIR/completion.zsh
+fi
+
+# Otherwise, if we have a standalone binary somewhere,
+# use it with our own shell integration.
+if [[ -n "$SV_FZF_BIN" ]]; then
+    source $SVCONFIGDIR/zsh/fzf/key-bindings.zsh
+    source $SVCONFIGDIR/zsh/fzf/completion.zsh
 fi
