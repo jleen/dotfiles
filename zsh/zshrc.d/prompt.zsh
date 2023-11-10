d325cc5f0a7b5f10543a8b580a8e2531e7513633
John Leen <jleen@saturnvalley.org>
2023-11-10 15:24:22 -0500

    Lighter and lighter

--- zsh/zshrc.d/prompt.zsh
+++ zsh/zshrc.d/prompt.zsh
@@ -4,12 +4,15 @@
 
 if [[ -n $WINDOW ]]; then
     local screen=" $WINDOW"
-    local screen_clear="`print \\\\ek\\\\e\\\\134`"  # ???
+    # https://www.gnu.org/software/screen/manual/html_node/Control-Sequences.html
+    # ... but do we need this?  What happens without it?
+    local screenclear="%{`print \\\\ek\\\\e\\\\134`%}"
 fi
-if [[ $TERM = (xterm*|rxvt*|screen|cygwin) ]]; then
-    local xterm="`print \\\\e`]0;$SHELLPREFIX${screen}:\${SV_SPECIAL_WINDOW_PROMPT}%~`print \\\\007`"
+
+if [[ $TERM = (xterm*|rxvt*|screen) ]]; then
+    local title="%{`print \\\\e`]0;$screen%~`print \\\\007`%}"
 fi
-local title="%{${screen_clear}${xterm}%}"
+
 local newline=$'\n'
 setopt prompt_subst
-PROMPT="$title$newline$prefix%B%F{cyan}%~%f%b$screen$newline%B%F{blue}$starboat%f%b "
+PROMPT="$screenclear$title$newline%B%F{cyan}%~%f%b$screen$newline%B%F{blue}$starboat%f%b "
