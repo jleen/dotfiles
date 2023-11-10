f7cd0a7056195a0c66345b3db095bda425c7ad06
John Leen <jleen@saturnvalley.org>
2023-11-10 15:29:05 -0500

    Commentary track

--- zsh/zshrc.d/prompt.zsh
+++ zsh/zshrc.d/prompt.zsh
@@ -1,7 +1,9 @@
 # Saturn Valley Intergalactic Standard Command Prompt
 
+# Distinguished funny symbol for root shell.
 [[ ${(%):-%#} = \# ]] && local starboat="#" || local starboat="❯"
 
+# GNU Screen identification.
 if [[ -n $WINDOW ]]; then
     local screen=" $WINDOW"
     # https://www.gnu.org/software/screen/manual/html_node/Control-Sequences.html
@@ -9,10 +11,16 @@ if [[ -n $WINDOW ]]; then
     local screenclear="%{`print \\\\ek\\\\e\\\\134`%}"
 fi
 
+# Window title.
 if [[ $TERM = (xterm*|rxvt*|screen) ]]; then
     local title="%{`print \\\\e`]0;$screen%~`print \\\\007`%}"
 fi
 
+# Hack around annoying shell quote behavior.
 local newline=$'\n'
+
+# Needed so we can pass $SV_PROMPT_EXTRA by name.
 setopt prompt_subst
+
+# Put it all together.
 PROMPT="$screenclear$title$newline%B%F{cyan}%~%f%b$screen\$SV_PROMPT_EXTRA$newline%B%F{blue}$starboat%f%b "
