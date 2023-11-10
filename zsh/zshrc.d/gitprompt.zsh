ba8fa4cc157acad3d25eb9ce646daa1464b4f68b
John Leen <jleen@saturnvalley.org>
2023-11-10 15:03:04 -0500

    Further decrufting

--- zsh/zshrc.d/gitprompt.zsh
+++ zsh/zshrc.d/gitprompt.zsh
@@ -3,8 +3,6 @@ if [[ ! -x "$SV_STARSHIP_BIN" ]]; then
     typeset -ga precmd_functions
     typeset -ga chpwd_functions
     typeset -ga preexec_functions
-    typeset -ga sv_special_prompt_specs
-
 
     function set_git_prompt {
       local gitcolorstart gitcolorend
@@ -45,37 +43,4 @@ if [[ ! -x "$SV_STARSHIP_BIN" ]]; then
     preexec_functions+='zsh_git_prompt_preexec'
 
     PR_GIT_UPDATE=1
-
-    function sv_set_special_prompt {
-      unset SV_SPECIAL_PROMPT
-      unset SV_SPECIAL_WINDOW_PROMPT
-      unset SV_PWD_PROMPT
-
-      for prompt_spec in $sv_special_prompt_specs; do
-        local prompt_trigger=`echo $prompt_spec|cut -d: -f1`
-        local prompt_name=`echo $prompt_spec|cut -d: -f2`
-        if [[ $PWD == $prompt_trigger* ]]; then
-          SV_SPECIAL_PROMPT="%{`tput bold`%}$prompt_name%{`tput sgr0`%}:"
-          SV_SPECIAL_WINDOW_PROMPT="$prompt_name:"
-          SV_PWD_PROMPT=`echo $PWD | cut -c$((${#prompt_trigger}+2))-`
-          SV_PWD_PROMPT=${SV_PWD_PROMPT:=.}
-        fi
-      done
-    }
-
-    function sv_set_prompt_precmd {
-      if [[ -n "$SV_PROMPT_UPDATE" ]] ; then
-        sv_set_special_prompt
-        SV_PROMPT_UPDATE=
-      fi
-    }
-    precmd_functions+='sv_set_prompt_precmd'
-
-    function sv_set_prompt_chpwd {
-      SV_PROMPT_UPDATE=1
-    }
-    chpwd_functions+='sv_set_prompt_chpwd'
-
-    SV_PROMPT_UPDATE=1
-
 fi
--- zsh/zshrc.d/prompt.zsh
+++ zsh/zshrc.d/prompt.zsh
@@ -1,17 +1,15 @@
 # Saturn Valley Intergalactic Standard Command Prompt
 
-local starboat="❯"
+[[ ${(%):-%#} = \# ]] && local starboat="#" || local starboat="❯"
 
 if [[ -n $WINDOW ]]; then
     local screen=" $WINDOW"
-    local screen_clear="`print \\\\ek\\\\e\\\\134`"
+    local screen_clear="`print \\\\ek\\\\e\\\\134`"  # ???
 fi
 if [[ $TERM = (xterm*|rxvt*|screen|cygwin) ]]; then
-    local xterm="`print \\\\e`]0;$SHELLPREFIX${screen}:\${SV_SPECIAL_WINDOW_PROMPT}\${SV_PWD_PROMPT:-%~}`print \\\\007`"
+    local xterm="`print \\\\e`]0;$SHELLPREFIX${screen}:\${SV_SPECIAL_WINDOW_PROMPT}%~`print \\\\007`"
 fi
-local sv_prompt_title="%{${screen_clear}${xterm}${shellprefix}%}"
-local color_on="%{$tput_setaf$tput_bold%}"
-local color_off="%{$tput_sgr0%}"
-setopt prompt_subst
+local title="%{${screen_clear}${xterm}%}"
 local newline=$'\n'
-PROMPT="$sv_prompt_title$newline$prefix%B%F{cyan}%~%f%b$screen$newline%B%F{blue}$starboat%f%b "
+setopt prompt_subst
+PROMPT="$title$newline$prefix%B%F{cyan}%~%f%b$screen$newline%B%F{blue}$starboat%f%b "
