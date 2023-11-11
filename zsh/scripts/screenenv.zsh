281e56f484b4111a039ca5f6d0f5e278ff319c6a
John Leen <jleen@saturnvalley.org>
2023-11-11 10:35:11 -0500

    Only screenenv cares about svtmp

--- zsh/scripts/screenenv.zsh
+++ zsh/scripts/screenenv.zsh
@@ -1,6 +1,28 @@
 # Screen Meta-Management
 
-SCREENENV=$SVTMP/screenenv
+# Private temp dir.
+SVTMP=$TMPPREFIX-$USERNAME-sv
+if [[ -e $SVTMP ]]; then
+  if [[ ! ( -O $SVTMP && -d $SVTMP ) ]]; then
+    echo "Bad $SVTMP"
+    unset SVTMP
+  fi
+else
+  if mkdir -pm 700 "$SVTMP" 2> /dev/null; then
+    echo "Created $SVTMP"
+  elif [[ -O $SVTMP && -d $SVTMP ]]; then
+    echo "Another process created $SVTMP for us, yay"
+  else
+    echo "Unable to create $SVTMP"
+    unset SVTMP
+  fi
+fi
+
+if [[ -d $SVTMP ]]; then
+  SCREENENV=$SVTMP/screenenv
+else
+  SCREENENV=$HOME/screenenv
+fi
 
 sv_exportable_vars=(
     DISPLAY XAUTHORITY DBUS_SESSION_BUS_ADDRESS SV_SUPPRESS_ITERM
