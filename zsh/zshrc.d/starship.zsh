bcc0a065e42ca033d16dbf8a5adabf51f4849b35
John Leen <jleen@saturnvalley.org>
2023-11-10 22:47:20 -0500

    Starship window title

--- zsh/zshrc.d/starship.zsh
+++ zsh/zshrc.d/starship.zsh
@@ -1,2 +1,14 @@
 export STARSHIP_CONFIG="$SVCONFIGDIR/starship/starship.toml"
-[[ -x "$SV_STARSHIP_BIN" ]] && eval "$($SV_STARSHIP_BIN init zsh)"
+if [[ -x "$SV_STARSHIP_BIN" ]]; then
+    eval "$($SV_STARSHIP_BIN init zsh)"
+    typeset -ga precmd_functions
+
+    function sv_prompt_window_title {
+        local ssh screen
+        [[ -n $SSH_CONNECTION ]] && ssh="$HOST "
+        [[ -n $WINDOW ]] && screen="$WINDOW "
+        print -Pn "\e]0;$ssh$screen%~\007"
+    }
+    precmd_functions+='sv_prompt_window_title'
+    sv_prompt_window_title
+fi
