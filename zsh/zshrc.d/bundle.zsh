f54ee808a1b4137f1718643dc73ec2a433ff2652
John Leen <jleen@saturnvalley.org>
2024-10-30 16:13:05 -0400

    Delicious zsh highlighting

--- zsh/zshrc.d/bundle.zsh
+++ zsh/zshrc.d/bundle.zsh
@@ -1,3 +1,3 @@
-for bundle in "$SVCONFIGDIR/zsh/bundle/zsh-autosuggestions/zsh-autosuggestions.zsh" "$SVCONFIGDIR/zsh/bundle/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"; do
+for bundle in "$SVCONFIGDIR/zsh/bundle/zsh-autosuggestions/zsh-autosuggestions.zsh" "$SVCONFIGDIR/zsh/bundle/catppuccin-syntax-highlighting/themes/catppuccin_frappe-zsh-syntax-highlighting.zsh" "$SVCONFIGDIR/zsh/bundle/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"; do
     [ -f $bundle ] && source $bundle
 done
