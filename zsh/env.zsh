d2b01a39e30f0ffe0a8bb5f225c13ecff478361e
John Leen <jleen@saturnvalley.org>
2026-05-08 17:41:24 -0400

    Further deprecations

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,8 +1,2 @@
-# Sets up environment variables that we want even if this is a
-# noninteractive shell, e.g. variables that we'd like to have available in
-# processes launched by wrapper scripts.
-
 [[ -x /usr/bin/lesspipe ]] && eval "$(lesspipe)"  # Set env vars for buffering.
-export POETRY_VIRTUALENVS_IN_PROJECT=true
-export PIPENV_VENV_IN_PROJECT=1
 export OPEN_SOURCE_CONTRIBUTOR=true  # Silence node nag.
