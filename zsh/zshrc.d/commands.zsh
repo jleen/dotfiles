8107d03096d2d46f360965e2bcb16fc724e3cc30
John Leen <jleen@saturnvalley.org>
2026-05-08 17:44:57 -0400

    Deprecate aliases

--- zsh/zshrc.d/commands.zsh
+++ zsh/zshrc.d/commands.zsh
@@ -14,13 +14,3 @@ fi
 
 # Sort "top" by CPU use on OS X, where for some reason this isn't the default.
 [[ $OSTYPE = darwin* ]] && alias top='top -o cpu'
-
-# Show a man page in a gvim window.
-# TODO(jleen): Make this part of v.zsh and make it work on OS X.
-gman () { man $* | col -b | gview - }
-
-# Recursively grep the current directory, ignoring VC metadata and caches.
-srcgrep () {
-  grep -r "$*" . | grep -v tags | grep -v \\.svn| grep -v \\.git \
-      | grep -v "Binary file"
-}
