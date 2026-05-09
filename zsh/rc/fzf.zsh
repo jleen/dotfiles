b21410d4afb06d90ada58b79045a2c6b9761b2e4
John Leen <jleen@saturnvalley.org>
2026-05-08 22:21:36 -0400

    local fzf

--- zsh/rc/fzf.zsh
+++ zsh/rc/fzf.zsh
@@ -1,3 +1,3 @@
-try_set_bin SV_FZF_BIN /usr/bin/fzf /opt/homebrew/bin/fzf
+try_set_bin SV_FZF_BIN ~/.local/bin/fzf /usr/bin/fzf /opt/homebrew/bin/fzf
 
 [[ -n $SV_FZF_BIN ]] && source <($SV_FZF_BIN --zsh)
